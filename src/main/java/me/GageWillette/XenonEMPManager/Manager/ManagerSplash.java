3bfae802a387812cd9c6888d2fff53ffb85c2c79
Gage <gagew0513@gmail.com>
2022-01-16 19:48:23 -0500

    Update 1/16

--- src/main/java/me/GageWillette/XenonEMPManager/Manager/ManagerSplash.java
+++ src/main/java/me/GageWillette/XenonEMPManager/Manager/ManagerSplash.java
@@ -3,6 +3,9 @@ package me.GageWillette.XenonEMPManager.Manager;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -10,38 +13,41 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;  //REMOVE ON PRODUCTION BUILD THIS IS SO BAD FOR MEMEMEMEMEMMEORY
 
 import ij.IJ;
 import ij.ImagePlus;
+import me.GageWillette.XenonEMPManager.Utils.ImagePanel;
 import me.GageWillette.XenonEMPManager.Utils.SQL;
 
-public class ManagerSplash extends JFrame{
+public class ManagerSplash extends JFrame {
 
 	JButton exit = new JButton("Exit");
 
 	private Connection con = this.getCon();
 
-	//Graphics graphics = getGraphics();
+//	Graphics graphics = getGraphics();
 	public ManagerSplash() {
 
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
-		} catch (ClassNotFoundException e) {e.printStackTrace();}
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
 
 		this.getLogins();
 
 		this.setBackground(Color.BLACK);
 
 		addExitButton();
-		
+
 		setLayouts();
 
 
 	}
 
-	private void setLayouts()
-	{
+	private void setLayouts() {
 		this.setLayout(null);
 
 		this.setAlwaysOnTop(true);
@@ -52,35 +58,42 @@ public class ManagerSplash extends JFrame{
 
 		this.setVisible(true);
 	}
-	
-	private void addExitButton()
-	{
+
+	private void addExitButton() {
 		exit.setBounds(1850, 1060, 70, 20);
-		
+
 		exit.addActionListener(new ExitButtonListener());
-		
+
 		exit.setVisible(true);
-		
+
 		this.add(exit);
 	}
-	private class ExitButtonListener implements ActionListener{
+
+	private class ExitButtonListener implements ActionListener {
 
 		public void actionPerformed(ActionEvent e) {
 			System.exit(0);
-			
+
 		}
 	}
 
-	private static Connection getCon()
-	{
+	private static Connection getCon() {
 		Connection temp = null;
 
 		try {
 			temp = SQL.getConnection("admin", "admin", "10.0.0.39", "5353");
-		} catch (SQLException e) {e.printStackTrace();}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 
 		return temp;
 	}
+
+	private void setBackground() throws IOException {
+		BufferedImage img = ImageIO.read(new File("src/main/java/logo.png"));
+		this.setContentPane(new ImagePanel(img));
+	}
+
 
 	private HashMap getLogins()
 	{
